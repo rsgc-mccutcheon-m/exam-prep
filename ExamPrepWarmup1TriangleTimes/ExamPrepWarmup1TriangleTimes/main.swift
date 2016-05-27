846b1ff3dede2c953bfcfb9407bebd5d4286891b
Mark McCutcheon <mmccutcheon@rsgc.on.ca>
2016-05-27 09:51:23 -0400

    Setup the repo

---
+++ ExamPrepWarmup1TriangleTimes/ExamPrepWarmup1TriangleTimes/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  ExamPrepWarmup1TriangleTimes
+//
+//  Created by Mark on 2016-05-27.
+//  Copyright © 2016 Clutch Industries. All rights reserved.
+//
+
+import Foundation
+
+
+
