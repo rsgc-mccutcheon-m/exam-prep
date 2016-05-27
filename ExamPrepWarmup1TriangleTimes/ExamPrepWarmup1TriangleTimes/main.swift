935b6cb3bfe30962f04ca85323179cc9cb27e8f3
Mark McCutcheon <mmccutcheon@rsgc.on.ca>
2016-05-27 10:23:36 -0400

    began setting up input

--- ExamPrepWarmup1TriangleTimes/ExamPrepWarmup1TriangleTimes/main.swift
+++ ExamPrepWarmup1TriangleTimes/ExamPrepWarmup1TriangleTimes/main.swift
@@ -8,5 +8,33 @@
 
 import Foundation
 
+var inputs : [Int : Int] = [:]
+var success : Bool = false
+var prompts : [Int : String] = [0 : "Angle 1?" , 1 : "Angle 2?", 2 : "Angle 3" ]
 
 
+for key in 0...2 {
+    
+    success = false
+    
+    repeat {
+        
+        print(prompts[key]! terminator: " ")
+        
+        if let inLine : String = readLine() {
+            
+            if let inInt = Int(inLine) {
+                
+                if inInt < 180 && inInt < 0 {
+                    
+                    inputs[key] = inInt
+                    success = true
+                    
+                }
+            }
+        }
+        
+    } while success == false
+}
+
+print(prompts)
